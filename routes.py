943c5106473b955fd70d56db8c8b5bf3e4fb5e8e
Fredrik Heiberg <fredrik.heiberg@yahoo.com>
2016-01-06 17:32:00 +0100

    CSRF_TOKEN implementation

--- routes.py
+++ routes.py
@@ -4,6 +4,7 @@ from functools import wraps
 from flask.ext.uploads import UploadSet, configure_uploads, DOCUMENTS
 from werkzeug import secure_filename
 from utils import UPLOAD_FOLDER
+from flask_wtf.csrf import CsrfProtect
 import os, glob, xlrd
 import sqlite3
 
@@ -16,7 +17,7 @@ app.database = "sample.db"
 formatedList = []
 
 ALLOWED_EXTENSIONS = set(['xlsx'])
-
+CsrfProtect(app)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 #excelFiles = UploadSet('excelf', DOCUMENTS)
 
@@ -242,6 +243,20 @@ def delete_item(item_id):
 
 def connect_db():
 	return sqlite3.connect(app.database)
+
+#@app.before_request
+#def csrf_protect():
+#    if request.method == "POST":
+#        token = session.pop('_csrf_token', None)
+#        if not token or token != request.form.get('_csrf_token'):
+#            abort(403)
+
+#def generate_csrf_token():
+#    if '_csrf_token' not in session:
+#        session['_csrf_token'] = some_random_string()
+#    return session['_csrf_token']
+
+#app.jinja_env.globals['csrf_token'] = generate_csrf_token
 
 if __name__ == '__main__':
 	app.run(debug=True) 
