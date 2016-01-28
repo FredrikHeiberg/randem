70f8f4167cbd3dd0e364ee46f851a65752e1f8c3
Fredrik Heiberg <fredrik.heiberg@yahoo.com>
2016-01-28 20:14:17 +0100

    new secret key

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ import os
 # Default config
 class BaseConfig(object):
 	DEBUG = False
-	SECRET_KEY = os.urandom(24)
+	#SECRET_KEY = os.urandom(24)
+	SECRET_KEY = 'm\xc0>c"\x89D\xa3HM\x8a+\xdc6\xd3\xff\x93\t\x86\x0b\xc8q\xe6&'
 	BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 	print SQLALCHEMY_DATABASE_URI
