644e7b5d7e2a53759abc83ae5dc574270ba7ace7
Fredrik Heiberg <fredrik.heiberg@yahoo.com>
2016-01-28 19:56:16 +0100

    js path fix

--- routes.py
+++ routes.py
@@ -2,8 +2,8 @@
 from flask import Flask, render_template, request, url_for, redirect, session, flash, g #, abort, send_from_directory
 from functools import wraps
 from flask.ext.uploads import UploadSet, configure_uploads, DOCUMENTS
-from xlrd import *
-from xlutils.copy import copy
+#from xlrd import *
+#from xlutils.copy import copy
 #from flask.ext.wtf import form
 #from form import LoginForm
 from werkzeug import secure_filename
@@ -16,7 +16,7 @@ from xlutils.filter import process,XLRDReader,XLWTWriter
 #from models import User
 
 #from models import *
-from flask.ext.sqlalchemy import SQLAlchemy
+#from flask.ext.sqlalchemy import SQLAlchemy
 import json, requests, hashlib
 import logging
 import sys
